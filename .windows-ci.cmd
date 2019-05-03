aa4bc43addd02ca920682d3dcc0b59c02337527f
Guilherme Bicalho Saturnino <gbsa@win.dtu.dk>
2019-05-03 15:06:11 +0200

    improvements to windows CI

--- .windows-ci.cmd
+++ .windows-ci.cmd
@@ -2,10 +2,11 @@
 start /wait bitsadmin.exe /transfer JOB https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe %cd%\Miniconda3.exe
 call Miniconda3.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%cd%\MINICONDA
 call %cd%\MINICONDA\Scripts\activate
-call conda env create -f environment.yml
-call conda activate simnibs_installer
-call pip install pyqt5
+conda env create -f environment.yml
+conda activate simnibs_installer
+pip install pyqt5
+python install_simnibs.py -s --pre-release
 call compile.cmd
-dist\install_simnibs.exe -s --pre-release
+dist\install_simnibs.exe -h -s
 echo %errorlevel%
 type %LOCALAPPDATA%\SimNIBS\simnibs_install_log.txt
