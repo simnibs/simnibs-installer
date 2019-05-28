fca0ccfdeab4e3c45834d21e983d1ceeb76834fc
guilhermebs <guilhermebs@drcmr.dk>
2019-05-28 13:09:47 +0200

    moving CI to yml files

--- .macos-ci.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -e
-
-pip install -r requirements.txt
-./compile.sh
-dist/install_simnibs -s --pre-release
-mv dist/* .
-#zip -r install_simnibs_macOS.zip install_simnibs.app
-# for now, because we dont have a certificate, i will ship the executable instead of the app
-zip -r install_simnibs_macOS.zip install_simnibs
--- .windows-ci.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-pip install -r requirements.txt
-call compile.cmd
-dist\install_simnibs.exe -s --pre-release
-move dist\install_simnibs.exe dist\install_simnibs_windows.exe
-echo %errorlevel%
-type %LOCALAPPDATA%\SimNIBS\simnibs_install_log.txt
